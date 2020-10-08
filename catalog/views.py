b592a232bb800f3fc07ad197b9db936deb32a200
haiduk <hwlee91@gmail.com>
2020-10-08 02:26:45 -0400

    making detail views

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
+from django.views import generic
 
 # Create your views here.
 
@@ -16,12 +17,30 @@ def index(request):
   # the 'all()' is implied by default.
   num_authors = Author.objects.count()
 
+  # genres challenge
+  num_genres = Genre.objects.count()
+
   context = {
     'num_books': num_books,
     'num_instances': num_instances,
     'num_instances_available': num_instances_available,
     'num_authors': num_authors,
+    'num_genres': num_genres,
   }
 
   # render the HTML template index.html with the data in the context variable
   return render(request, 'index.html', context=context)
+
+class BookListView(generic.ListView):
+  model = Book
+  # paginate large data
+  paginate_by = 10
+
+class BookDetailView(generic.DetailView):
+  model = Book
+
+class AuthorListView(generic.ListView):
+  model = Author
+
+class BookDetailView(generic.DetailView):
+  model = Author
