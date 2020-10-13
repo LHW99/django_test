7102f55e61248ced24a608322d0118c51ff8d66f
haiduk <hwlee91@gmail.com>
2020-10-13 02:41:10 -0400

    django_heroku

--- django_test/settings.py
+++ django_test/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,6 +149,3 @@ STATIC_URL = '/static/'
 # simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-import django_heroku
-django_heroku.settings(locals())
