b5fc11a9dee5694cd8c1dddbc3fb82bfeb26b60f
haiduk <hwlee91@gmail.com>
2020-10-13 03:15:16 -0400

    dj_database_url

--- django_test/settings.py
+++ django_test/settings.py
@@ -127,17 +127,17 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Heroku: Update database configuration from $DATABASE_URL
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
-
-# static files (css, javascript, images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 # the absolute path to the directory where collectstatic will collect static files for deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
