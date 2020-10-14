0c23039e1bc337a09fc79ea76274ce0120725414
haiduk <hwlee91@gmail.com>
2020-10-13 22:35:59 -0400

    updated settings.py

--- django_test/settings.py
+++ django_test/settings.py
@@ -14,21 +14,21 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&60u*cy0ve3*a9$k3-(-)yi&ncdoy$-9!lbc_4!95(4bz$u@&1'
-import os
+# SECRET_KEY = '&60u*cy0ve3*a9$k3-(-)yi&ncdoy$-9!lbc_4!95(4bz$u@&1'
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '&60u*cy0ve3*a9$k3-(-)yi&ncdoy$-9!lbc_4!95(4bz$u@&1')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+# DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'django_test.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -132,6 +132,8 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+# added to test email
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
